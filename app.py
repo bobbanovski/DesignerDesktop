85dffadbf05c7c7e587d10bbe633c009193e5a1e
bobbanovski <bobbanovski@gmail.com>
2017-09-10 17:39:00 +1000

    Join QtDesigner template to python app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from PyQt5.QtWidgets import *
+import sys
+import pydownloader
+
+
+class MainProgram(QDialog, pydownloader.Ui_Dialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setupUi(self)
+
+        self.progress.setValue(50)
+        self.urledit.setText("http://www.google.com")
+
+app = QApplication(sys.argv)
+mainprogram = MainProgram()
+mainprogram.show()
+app.exec_()
